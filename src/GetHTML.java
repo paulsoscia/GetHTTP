9a63436571d7b842f600d6aa37946999d9a5907f
GIT Admin <paulsoscia@PennAssuranceSoftware.com>
2014-09-29 17:18:21 -0400

    command line parms using constructor class

--- src/GetHTML.java
+++ src/GetHTML.java
@@ -8,9 +8,13 @@ import java.net.URL;
 
 public class GetHTML {
 
-	String sURL					= "http://www.google.com/";	// A valid URL
+	static String  sURL					= "http://www.yahoo.com/";	// A valid URL
 	
 	public GetHTML() {
+		this(sURL);
+	}
+	
+	public GetHTML(String sLocalURL) {
 
 		String sHTTpResponseLine	= "";	// One line from the HTTP response
 		String sHTTpResponseLines	= "";	// Entire HTTP response
@@ -18,7 +22,7 @@ public class GetHTML {
 		
 		try {
 			iReturn = 1;
-			URL urlSiteToGetHTML = new URL(sURL);
+			URL urlSiteToGetHTML = new URL(sLocalURL);
 			try {
 				InputStream isGetHtmlData = urlSiteToGetHTML.openStream();
 				BufferedReader brGetHtmlData = new BufferedReader(new InputStreamReader(isGetHtmlData));
@@ -29,15 +33,15 @@ public class GetHTML {
 			} 
 			catch (IOException e) {
 				iReturn = 0;
-				System.out.println("Error:  Problem reading HTML, with url " + sURL + ". IOException");
+				System.out.println("Error:  Problem reading HTML, with url " + sLocalURL + ". IOException");
 			}
 		}
 		catch (MalformedURLException e) {
 			iReturn = 0;
-			System.out.println("Error:  Problem with url " + sURL + ". MalformedURLException");
+			System.out.println("Error:  Problem with url " + sLocalURL + ". MalformedURLException");
 		}
 		
-		System.out.println("Completed: sURL=" + sURL);
+		System.out.println("Completed: sURL=" + sLocalURL);
 		if (iReturn == 1)
 		{
 			System.out.println("Completed: HTTP get was successful");
@@ -47,11 +51,48 @@ public class GetHTML {
 				
 	}
 	
+	public static void commandlineParms(String[] args)
+	{
+		
+		for (String sCommandLineParameter: args) {
+			sCommandLineParameter = sCommandLineParameter.trim().toUpperCase();
+			String sLeftMostCommandLineParameter = sCommandLineParameter.substring(0,3);
+			if (sLeftMostCommandLineParameter.equals("HTT"))
+			{
+				sCommandLineParameter = "-U=" + sCommandLineParameter;
+				sLeftMostCommandLineParameter = sCommandLineParameter.substring(0,3);
+			}
+		    switch(sLeftMostCommandLineParameter){
+			    case "-U=": 
+			    	System.out.println("URLs");
+			    	break;
+			    case "-I=": 
+			    	System.out.println("Input File");
+			    	break;
+			    case "-O=": 
+			    	System.out.println("Input File");
+			    	break;
+			    default :
+			    	System.out.println("Invalid Number of Day");
+		     }
+		}
+	}
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		//	Add URL as command line parameter
 		//	Add Log4j
-		GetHTML getGooglePage = new GetHTML();
+		commandlineParms(args);
+		if (args.length == 0) {
+			// no/none command line parameters were provided
+			GetHTML getGooglePage = new GetHTML();			
+		}
+		else {
+			// command line parameters were provided
+			for (String sCommandLineParameter: args) {
+				GetHTML getCmdParmsPage = new GetHTML(sCommandLineParameter);
+			}
+		}
 
 	} // main end
 
