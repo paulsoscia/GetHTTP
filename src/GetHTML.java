8926dd2563e3b8cda96c7d1fb03f81c740607470
GIT Admin <paulsoscia@PennAssuranceSoftware.com>
2014-10-30 22:20:51 -0400

    starting with DB support and find the last business day and support for output file:

--- src/GetHTML.java
+++ src/GetHTML.java
@@ -1,14 +1,22 @@
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
+import java.io.Closeable;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
 
@@ -41,6 +49,15 @@ public class GetHTML {
 	private static		String		sFileNameDateFormat			= 	"yyyyMMdd_HH:mm:ss:SSS";
 	private static		Date   		dtFileNameDate				=	new Date();
 	private static		DateFormat	dfFileNameDateFormat		=	new SimpleDateFormat(sFileNameDateFormat);
+	
+	private static		String		sFileNameDateFormatYahooFinance			= 	"MM/d/yyyy";
+	private static		Date   		dtFileNameDateYahooFinance				=	new Date();
+	private static		DateFormat	dfFileNameDateFormatYahooFinance		=	new SimpleDateFormat(sFileNameDateFormat);
+	
+	private 			Connection			connect 			= null;
+	private 			Statement 			statement 			= null;
+	private 			PreparedStatement 	preparedStatement 	= null;
+	private 			ResultSet 			resultSet 			= null;
 	
 	public static String getsFileNameDateFormat() {
 		return sFileNameDateFormat;
@@ -82,9 +99,76 @@ public class GetHTML {
 		GetHTML.bOutputFile = bOutputFile;
 	}
 	
+	public void LastBusinessDay()
+	{
+		//TODO add holiday support ; add before 5PM logic to check previous business day
+		Boolean isHoliday; 
+		//List<Date> holidays = dia.getAllNationalHolidays();
+
+		// get the current date without the hours, minutes, seconds and millis
+		Calendar cal = Calendar.getInstance();		
+		
+		int dayOfWeek = cal.get(Calendar.DAY_OF_WEEK);
+		
+		if (dayOfWeek != Calendar.SUNDAY) {
+			cal.add(Calendar.DAY_OF_YEAR, -2);
+		}
+		if (dayOfWeek != Calendar.SATURDAY) {
+			cal.add(Calendar.DAY_OF_YEAR, -1);
+		}
+		
+	}
+	
+	public void createTables() throws Exception {
+	    try {
+	      // this will load the MySQL driver, each DB has its own driver
+	      Class.forName("com.mysql.jdbc.Driver");
+	      // setup the connection with the DB.
+	      connect = DriverManager.getConnection("jdbc:mysql://localhost/feedback?" + "user=sqluser&password=sqluserpw"); //"user=root&password=12many");
+
+	      
+	      // preparedStatements can use variables and are more efficient
+	      preparedStatement = connect.prepareStatement("create table mytable (a integer, b varchar(100), c long, prime varchar(2), tictac varchar(9) )");
+	      // "myuser, webpage, datum, summary, COMMENTS from FEEDBACK.COMMENTS");
+	      // parameters start with 1
+	      preparedStatement.executeUpdate();
+
+	      
+	    } catch (Exception e) {
+	    	//if (e.vendorCode == 1050)
+	    	{
+	    		log.error("SQL vendorCode=1050 " + e.getMessage());
+	    	}
+	    	throw e;
+	    } finally {
+	      close();
+	    }
+
+	  }
+		
+
+		  // you need to close all three to make sure
+		  private void close() {
+		    //close(resultSet);
+		    //close(statement);
+		    //close(connect);
+		  }
+		  
+		  public void close(Closeable c) {
+		    try {
+		      if (c != null) {
+		        c.close();
+		      }
+		    } catch (Exception e) {
+		    // don't throw now as it might leave following closables in undefined state
+		    }
+		  }	
+	// END   JDBC
+	
 	public void writeFile(String sPathAndFileName, String sDataToWriteToFile) {
 	
 		dfFileNameDateFormat = new SimpleDateFormat(sFileNameDateFormat);
+		dfFileNameDateFormatYahooFinance = new SimpleDateFormat(sFileNameDateFormatYahooFinance);
 		sPathAndFileName = sPathAndFileName.replace(".", dfFileNameDateFormat.format(dtFileNameDate) + ".");
 		
 		BufferedWriter writer = null;
@@ -105,7 +189,7 @@ public class GetHTML {
 		    try
 		    {
 		        if ( writer != null)
-		        	writer.close( );
+		        	writer.close();
 		    }
 		    catch ( IOException e)
 		    {
@@ -187,6 +271,7 @@ public class GetHTML {
 	    	log.debug("using the default command line parms with default URLS=" + sURL);
 	    	listCommandLineParmsTemp.add(sURL);
 	    }
+	    
 	    // nonBlank will have all the elements which contain some characters.
 	    String[] strArrCommandLineParms = (String[]) listCommandLineParmsTemp.toArray( new String[listCommandLineParmsTemp.size()] );
 		
@@ -237,13 +322,13 @@ public class GetHTML {
 		//String log4jConfPath = "/home/paul/workspace/dev/GetHTML/properties/log4j.properties";
 		//PropertyConfigurator.configure(log4jConfPath);
 		log.info("Time: Starting (main)" );
-	    	    
-		String[] sListURLs = commandlineParms(args);
 		
-		GetHTML getCmdParmsPage = new GetHTML();
+		String[] sListURLs = commandlineParms(args);
+		GetHTML getHtmlgetPage = new GetHTML();
+		
 		for (String sCommandLineParameter: sListURLs) {
 			log.info("Time: Staring getHTML (main) Starting " + sCommandLineParameter );
-			getCmdParmsPage.getHTML(sCommandLineParameter);
+			getHtmlgetPage.getHTML(sCommandLineParameter);
 			log.info("Time: Ending  getHTML (main) Ending   " + sCommandLineParameter);
 		}
 		log.info("Time: Ending   (main)" );
